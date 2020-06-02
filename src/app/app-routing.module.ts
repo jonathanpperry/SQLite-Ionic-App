ad24443de6faf82515284086643d967ca3589934
mc-perry <jonathan@m-craft.com>
2020-06-02 12:52:29 +0900

    First commit of respository with all necessary libraries installed

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'developers',
+    loadChildren: './pages/developers/developers.module#DevelopersPageModule',
+  },
+  {
+    path: 'developers/:id',
+    loadChildren: './pages/developer/developer.module#DeveloperPageModule',
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
