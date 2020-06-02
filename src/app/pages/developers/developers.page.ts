f0f3912051285fee5c5f8e247200be29dec85dfa
mc-perry <jonathan@m-craft.com>
2020-06-02 13:00:45 +0900

    Finished the code but it still doesn't build

--- src/app/pages/developers/developers.page.ts
+++ src/app/pages/developers/developers.page.ts
@@ -1,4 +1,6 @@
+import { DatabaseService, Dev } from './../../services/database.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-developers',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./developers.page.scss'],
 })
 export class DevelopersPage implements OnInit {
+  developers: Dev[] = [];
 
-  constructor() { }
+  products: Observable<any[]>;
+
+  developer = {};
+  product = {};
+
+  selectedView = 'devs';
+
+  constructor(private db: DatabaseService) {}
 
   ngOnInit() {
+    this.db.getDatabaseState().subscribe((rdy) => {
+      if (rdy) {
+        this.db.getDevs().subscribe((devs) => {
+          this.developers = devs;
+        });
+        this.products = this.db.getProducts();
+      }
+    });
   }
 
+  addDeveloper() {
+    let skills = this.developer['skills'].split(',');
+    skills = skills.map((skill) => skill.trim());
+
+    this.db
+      .addDeveloper(this.developer['name'], skills, this.developer['img'])
+      .then((_) => {
+        this.developer = {};
+      });
+  }
+
+  addProduct() {
+    this.db
+      .addProduct(this.product['name'], this.product['creator'])
+      .then((_) => {
+        this.product = {};
+      });
+  }
 }
